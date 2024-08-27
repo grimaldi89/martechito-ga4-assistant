23c59e356f9157814b9ded9066ca013c129563a3
Rodolfo <rodolfogrimaldi15@gmail.com>
2024-08-27 01:02:23 -0300

    added logo

--- src/streamlit_app/src/streamlit_app.py
+++ src/streamlit_app/src/streamlit_app.py
@@ -79,26 +79,56 @@ def main():
             # Atualize o rag_chain com o novo retriever
             rag_chain = chain(retriever=retriever, llm=llm, contextualize_q_system_prompt=CONTEXTUALIZE_Q_SYSTEM_PROMPT)
             
-        st.image("src/img/martechito-removebg-preview.png", use_column_width=True)
-        st.sidebar.markdown("<h2 style='text-align: center; margin-top: 0;'>GA4 AI Assistant</h2>", unsafe_allow_html=True)
+        st.image("src/img/martechito-logo.png", use_column_width=True)
+        language = st.sidebar.selectbox("Select Language", ["Português","English"])
+        # Conteúdo em inglês
+        about_text_en = f"""
+        ### About Martechito
+        Martechito is a specialized chatbot designed to streamline your experience with GA4, the latest iteration of Google Analytics. As your digital assistant, Martechito provides instant, accurate responses directly from GA4's official documentation and public knowledge base.
+
+        Powered by a state-of-the-art Retrieval-Augmented Generation (RAG) model, Martechito integrates OpenAI's GPT-4 with the Qdrant vector store to deliver contextually relevant answers to your inquiries.
+
+        Your insights and suggestions are invaluable. Connect with us on [LinkedIn]({LINKEDIN_URL}) or via email at martechito.assistant@gmail.com to share your feedback or contribute to the project's growth.
+        """
+
+        interactions_text_en = """
+        ### Interactions
+        Martechito is equipped to assist you with GA4's features, implementation strategies, and best practices. Whether you're setting up GA4 for the first time or need expert advice on advanced configurations, Martechito is here to help.
+
+        Please note: Martechito is currently in its MVP stage. While we strive for accuracy, some responses may be incomplete or require further refinement. We appreciate your understanding and encourage you to provide feedback via email at martechito.assistant@gmail.com to enhance its capabilities.
+        """
+
+
+        # Conteúdo em português
+        about_text_pt = f"""
+        ### Sobre o Martechito
+        O Martechito é um chatbot especializado, projetado para simplificar sua experiência com o GA4, a versão mais recente do Google Analytics. Como seu assistente digital, o Martechito fornece respostas instantâneas e precisas diretamente da documentação oficial do GA4 e da base de conhecimento pública.
+
+        Com a tecnologia de ponta do modelo de Geração Aumentada por Recuperação (RAG), o Martechito integra o GPT-4 da OpenAI com o armazenamento vetorial Qdrant para entregar respostas contextualmente relevantes às suas perguntas.
+
+        Suas percepções e sugestões são inestimáveis. Conecte-se conosco no [LinkedIn]({LINKEDIN_URL}) ou via e-mail em martechito.assistant@gmail.com para compartilhar seu feedback ou contribuir para o crescimento do projeto.
+        """
+
+        interactions_text_pt = """
+        ### Interações
+        O Martechito está equipado para auxiliá-lo com os recursos, estratégias de implementação e melhores práticas do GA4. Seja para configurar o GA4 pela primeira vez ou para obter conselhos especializados sobre configurações avançadas, o Martechito está aqui para ajudar.
+
+        Observe que o Martechito está atualmente em sua fase de MVP. Embora nos esforcemos para oferecer respostas precisas, algumas podem estar incompletas ou precisar de refinamento. Agradecemos sua compreensão e incentivamos você a fornecer feedback via e-mail em martechito.assistant@gmail.com para aprimorar suas capacidades.
+        """
+
+        st.sidebar.markdown("<h2 style='text-align: center; margin-top: 0;'>Martechito <br> GA4 Assistant</h2>", unsafe_allow_html=True)
         st.sidebar.markdown("---")
-        st.sidebar.markdown(f"""
-            ### About
-            This chatbot is a personal project created to answer questions about GA4, offering a conversational interface to its official documentation and public knowledge base.
+        if language == "English":
+            st.sidebar.markdown(about_text_en)
+            st.sidebar.markdown(interactions_text_en)
+        else:
+            st.sidebar.markdown(about_text_pt)
+            st.sidebar.markdown(interactions_text_pt)
 
-            It leverages a Retrieval-Augmented Generation (RAG) model that integrates OpenAI's GPT-4 with a Qdrant vector store to provide relevant responses to user inquiries.
-
-            Feedback and contributions are welcome! Feel free to reach out to me on [LinkedIn]({LINKEDIN_URL}) or [GitHub]({GITHUB_URL}).
-        """)
-        st.sidebar.markdown("""
-            ### Interactions
-            The chatbot is designed to provide information about GA4, the latest version of Google Analytics. It can answer questions about GA4's features, implementation and best practices.
-
-            Ps: The chatbot is still in development, so it may not have all the answers you're looking for or may provide incorrect information. Please use it at your own discretion.
-        """)
+        st.sidebar.markdown("---")
         
         st.markdown(f"<a href='{LINKEDIN_URL}'><img src='{LINKEDIN_IMAGE}' style='height:50px; margin-right: 10px;'></a>"
-                    f"<a href='{GITHUB_URL}'><img src='{GITHUB_IMAGE}' style='height:50px;'></a>", unsafe_allow_html=True)
+                    , unsafe_allow_html=True)
     
     with st.chat_message("assistant"):
         st.markdown("My name is Martechito, GA4 AI assistant, how can I help you today?")
